2da10c899d72519b64c0fda93d245cec90aad811
Anjana V U <103420152+anjanavu@users.noreply.github.com>
2025-05-20 18:47:29 +0530

    #subscription

--- src/routes/PrivateRouter.jsx
+++ src/routes/PrivateRouter.jsx
@@ -2,5 +2,5 @@ import { Navigate } from "react-router-dom";
 
 export const PrivateRoute = ({ children }) => {
   const isAuth = localStorage.getItem("4ZbFyHHg8uVrN5mP9kL3JhH7");
-  return isAuth ? children : <Navigate to="/members" />;
+  return isAuth ? children : <Navigate to="/" />;
 };
